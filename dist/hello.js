0a0feecc97cf86fd2563e77a9e0b4ac89c126f24
okakyo <0622okakyo@gmail.com>
2019-06-04 21:08:01 +0900

    GAS設計中

--- dist/hello.js
+++ dist/hello.js
@@ -1,13 +1,39 @@
+"use strict";
+const ACCESS_TOKEN = PropertiesService.getScriptProperties().getProperty("LINE_API_TOKEN");
+const baseURL = "https://api.line.me/v2/bot/";
 function doGet() {
-  var html = HtmlService.createTemplateFromFile("index");   
-  return html.evaluate();
+    let html = HtmlService.createTemplateFromFile("index");
+    return html.evaluate();
 }
- 
-function getData(alg){
-  if(alg == "title"){
-    return "Hello!";
-  }else if(alg == "body"){
-    return "これはHTMLページの本文に表示するテキストです";
-  }
+function getData(alg) {
+    if (alg == "title") {
+        return "Hello!";
+    }
+    else if (alg == "body") {
+        return "これはHTMLページの本文に表示するテキストです";
+    }
 }
-
+function doPost(e) {
+    // WebHookで受信した応答用Token
+    var replyToken = JSON.parse(e.postData.contents).events[0].replyToken;
+    // ユーザーのメッセージを取得
+    var userMessage = JSON.parse(e.postData.contents).events[0].message.text;
+    // 応答メッセージ用のAPI URL
+    var url = 'message/reply';
+    UrlFetchApp.fetch(url, {
+        'headers': {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer ' + ACCESS_TOKEN,
+        },
+        'method': 'post',
+        'payload': JSON.stringify({
+            'replyToken': replyToken,
+            'messages': [{
+                    'type': 'text',
+                    'text': userMessage + 'ンゴ',
+                }],
+        }),
+    });
+    return ContentService.createTextOutput(JSON.stringify({ 'content': 'post ok' })).setMimeType(ContentService.MimeType.JSON);
+}
+//# sourceMappingURL=hello.js.map
--- hello.ts
+++ hello.ts
@@ -1,4 +1,43 @@
-function HelloGAS(){
-    Logger.log('Hello GAS from VSCode!');
+const  ACCESS_TOKEN:string|null = PropertiesService.getScriptProperties().getProperty("LINE_API_TOKEN");
+const baseURL:string="https://api.line.me/v2/bot/"
+function doGet() {
+    let  html = HtmlService.createTemplateFromFile("index");   
+    return html.evaluate();
 }
-HelloGAS();
+   
+function getData(alg:string){
+    if(alg == "title"){
+      return "Hello!";
+    }
+    else if(alg == "body"){
+      return "これはHTMLページの本文に表示するテキストです";
+    }
+}
+  
+
+
+
+function doPost(e:any) {
+    // WebHookで受信した応答用Token
+    var replyToken:string = JSON.parse(e.postData.contents).events[0].replyToken;
+    // ユーザーのメッセージを取得
+    var userMessage:string = JSON.parse(e.postData.contents).events[0].message.text;
+    // 応答メッセージ用のAPI URL
+    var url:string = 'message/reply';
+  
+    UrlFetchApp.fetch(url, {
+      'headers': {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ' + ACCESS_TOKEN,
+      },
+      'method': 'post',
+      'payload': JSON.stringify({
+        'replyToken': replyToken,
+        'messages': [{
+          'type': 'text',
+          'text': userMessage + 'ンゴ',
+        }],
+      }),
+      });
+    return ContentService.createTextOutput(JSON.stringify({'content': 'post ok'})).setMimeType(ContentService.MimeType.JSON);
+  }
