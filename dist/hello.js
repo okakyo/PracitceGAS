fb4ff953c6ed67b1a072dd0e24eb2077ad02cf29
okakyo <0622okakyo@gmail.com>
2019-06-04 17:09:01 +0900

    提出

--- dist/hello.js
+++ dist/hello.js
@@ -1,6 +1,13 @@
-"use strict";
-function HelloGAS() {
-    Logger.log('Hello GAS from VSCode!');
+function doGet() {
+  var html = HtmlService.createTemplateFromFile("index");   
+  return html.evaluate();
 }
-HelloGAS();
-//# sourceMappingURL=hello.js.map
+ 
+function getData(alg){
+  if(alg == "title"){
+    return "Hello!";
+  }else if(alg == "body"){
+    return "これはHTMLページの本文に表示するテキストです";
+  }
+}
+
