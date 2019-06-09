208c91fdd055d2611d6efcc8415e46d05e71f432
okakyo <0622okakyo@gmail.com>
2019-06-09 12:38:03 +0900

    提出

---
+++ line.ts
@@ -0,0 +1,31 @@
+function doPost(e:any) {
+    // WebHookで受信した応答用Token
+    var replyToken:string = JSON.parse(e.postData.contents).events[0].replyToken;
+    // ユーザーのメッセージを取得
+    var userMessage:string = JSON.parse(e.postData.contents).events[0].message.text;
+    // 応答メッセージ用のAPI URL
+    var url:string = baseURL+'message/reply';
+  
+    interface beforeHeaders{
+      'headers':{[keys:string]:string},
+      'method':any,
+      'payload':{[key:string]:string}
+    }
+    
+    const Headers:beforeHeaders={
+      'headers': {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ' + ACCESS_TOKEN,
+      },
+      'method': "post",
+      'payload':{
+        'replyToken':replyToken,
+        'message':JSON.stringify({
+          'type': 'text',
+          'text': userMessage + 'ンゴ', 
+        })
+      }
+    }
+    UrlFetchApp.fetch(url, Headers);
+    return ContentService.createTextOutput(JSON.stringify({'content': 'post ok'})).setMimeType(ContentService.MimeType.JSON);
+  }
